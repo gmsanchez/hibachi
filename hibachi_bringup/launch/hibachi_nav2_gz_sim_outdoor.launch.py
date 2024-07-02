06d4d216cb59b4e597b50debc4d4b079f9104725
Guido Sanchez <sanchez.guido@gmail.com>
2024-07-02 15:57:00 +0000

    Add outdoor navigation using GPS and gz_sim

---
+++ hibachi_bringup/launch/hibachi_nav2_gz_sim_outdoor.launch.py
@@ -0,0 +1,118 @@
+# import os
+
+# from ament_index_python.packages import get_package_share_directory
+# from launch.launch_description_sources import PythonLaunchDescriptionSource
+
+from launch.actions import DeclareLaunchArgument, IncludeLaunchDescription
+from launch.substitutions import LaunchConfiguration, PathJoinSubstitution
+from launch.launch_description_sources import PythonLaunchDescriptionSource
+from launch_ros.substitutions import FindPackageShare
+from launch import LaunchDescription
+
+
+def generate_launch_description():
+
+   use_sim_time = LaunchConfiguration('use_sim_time', default='true')
+
+   gazebo_gps_world_launch = IncludeLaunchDescription(
+      PythonLaunchDescriptionSource(
+         PathJoinSubstitution(
+            [FindPackageShare("hibachi_gz"),
+             "launch",
+             "hibachi_gz_gps_world.launch.py"],
+         )
+      ),
+   )
+
+   dual_ekf_navsat_launch = IncludeLaunchDescription(
+      PythonLaunchDescriptionSource(
+         PathJoinSubstitution(
+            [FindPackageShare("hibachi_localization"),
+             "launch",
+             "dual_ekf_navsat.launch.py"],
+         )
+      ),
+      launch_arguments= {'use_sim_time': use_sim_time}.items(),
+   )
+   
+
+   teleop_joy_launch = IncludeLaunchDescription(
+      PythonLaunchDescriptionSource(
+         PathJoinSubstitution(
+            [FindPackageShare("hibachi_teleop"),
+             "launch",
+             "hibachi_teleop_twist_joy.launch.py"],
+         )
+      ),
+      launch_arguments= {'use_sim_time': use_sim_time}.items(),
+   )
+   
+   teleop_twist_stamper_launch = IncludeLaunchDescription(
+      PythonLaunchDescriptionSource(
+         PathJoinSubstitution(
+            [FindPackageShare("hibachi_teleop"),
+             "launch",
+             "hibachi_twist_stamper.launch.py"],
+         )
+      ),
+      launch_arguments= {'use_sim_time': use_sim_time}.items(),
+   )
+
+   teleop_twist_mux_launch = IncludeLaunchDescription(
+      PythonLaunchDescriptionSource(
+         PathJoinSubstitution(
+            [FindPackageShare("hibachi_teleop"),
+             "launch",
+             "hibachi_twist_mux.launch.py"],
+         )
+      ),
+      launch_arguments= {'use_sim_time': use_sim_time}.items(),
+   )
+
+#    nav2_localization_launch = IncludeLaunchDescription(
+#       PythonLaunchDescriptionSource(
+#          PathJoinSubstitution(
+#             [FindPackageShare("hibachi_navigation"),
+#              "launch",
+#              "localization.launch.py"],
+#          )
+#       ),
+#       launch_arguments= {'use_sim_time': use_sim_time}.items(),
+#    )
+
+
+   nav2_launch = IncludeLaunchDescription(
+      PythonLaunchDescriptionSource(
+         PathJoinSubstitution(
+            [FindPackageShare("hibachi_navigation"),
+             "launch",
+             "nav2.launch.py"],
+         )
+      ),
+      launch_arguments= {'use_sim_time': use_sim_time,
+                         'params': PathJoinSubstitution(
+                [FindPackageShare("hibachi_navigation"), "config", "nav2_no_map_params.yaml"])}.items(),
+   )
+
+   rviz_nav2_launch = IncludeLaunchDescription(
+      PythonLaunchDescriptionSource(
+         PathJoinSubstitution(
+            [FindPackageShare("hibachi_rviz"),
+             "launch",
+             "hibachi_rviz_nav2.launch.py"],
+         )
+      ),
+      launch_arguments= {'use_sim_time': use_sim_time,
+                         'rviz_config_file': PathJoinSubstitution(
+                [FindPackageShare("hibachi_rviz"), "rviz", "map_nav2.rviz"])}.items(),
+   )
+   
+   return LaunchDescription([
+      gazebo_gps_world_launch,
+      dual_ekf_navsat_launch,
+      # teleop_joy_launch,
+      teleop_twist_mux_launch,
+      teleop_twist_stamper_launch,
+      nav2_launch,
+      rviz_nav2_launch,
+   ])
