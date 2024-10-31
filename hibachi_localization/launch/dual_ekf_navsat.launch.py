b1057afa50471a739b721d00286264ecb1d70baa
Guido Sanchez <gsanchez@sinc.unl.edu.ar>
2024-10-31 13:06:23 -0300

    Add conditional imu_topic_name for simulation and real hardware.

--- hibachi_localization/launch/dual_ekf_navsat.launch.py
+++ hibachi_localization/launch/dual_ekf_navsat.launch.py
@@ -1,21 +1,40 @@
 from launch import LaunchDescription
-from launch.actions import DeclareLaunchArgument
+from launch.actions import DeclareLaunchArgument, SetLaunchConfiguration
 from launch_ros.actions import Node
-from launch.substitutions import LaunchConfiguration, PathJoinSubstitution
+from launch.substitutions import LaunchConfiguration, PathJoinSubstitution, PythonExpression
 
 from launch_ros.substitutions import FindPackageShare
 
 
+from launch.actions import LogInfo, DeclareLaunchArgument
+from launch.conditions import IfCondition
+from launch.substitutions import TextSubstitution, EqualsSubstitution, NotEqualsSubstitution, IfElseSubstitution
+  
+
 def generate_launch_description():
 
-    use_sim_time = LaunchConfiguration('use_sim_time', default='false')
+    use_sim_time = LaunchConfiguration('use_sim_time')
+    
+    imu_topic_name = IfElseSubstitution(
+                        LaunchConfiguration("use_sim_time"),
+                        if_value="/imu_filter_madgwick/imu/data",
+                        else_value="/mti_630_8A1G6/imu/data")
+    
     
     return LaunchDescription([
             DeclareLaunchArgument(
                 'use_sim_time',
-                default_value="false",
+                default_value='Flse',
                 description='Use simulation (Gazebo) clock if true'),
-            
+
+            LogInfo(condition=IfCondition(PythonExpression(['not ', use_sim_time])),
+                msg=['use_sim_time set to False. Using ', imu_topic_name]
+            ),
+
+            LogInfo(condition=IfCondition(use_sim_time),
+                msg=['use_sim_time set to True. Using ', imu_topic_name]
+            ),
+
             Node(
                 package="robot_localization",
                 executable="ekf_node",
@@ -24,7 +43,7 @@ def generate_launch_description():
                 parameters=[{"use_sim_time": use_sim_time},
                             PathJoinSubstitution([FindPackageShare('hibachi_localization'), 'config', 'dual_ekf_navsat_params.yaml'])],
                 remappings=[("odometry/filtered", "odometry/local"),
-                            ("imu/data", "/imu_filter_madgwick/imu/data")],
+                            ("imu/data", imu_topic_name)],
             ),
 
             Node(
@@ -35,7 +54,7 @@ def generate_launch_description():
                 parameters=[{"use_sim_time": use_sim_time},
                             PathJoinSubstitution([FindPackageShare('hibachi_localization'), 'config', 'dual_ekf_navsat_params.yaml'])],
                 remappings=[("odometry/filtered", "odometry/global"),
-                            ("imu/data", "/imu_filter_madgwick/imu/data")],
+                            ("imu/data", imu_topic_name)],
             ),
 
             Node(
@@ -46,11 +65,12 @@ def generate_launch_description():
                 parameters=[{"use_sim_time": use_sim_time},
                             PathJoinSubstitution([FindPackageShare('hibachi_localization'), 'config', 'dual_ekf_navsat_params.yaml'])],
                 remappings=[
-                    ("imu", "/imu_filter_madgwick/imu/data"),
+                    ("imu", imu_topic_name),
                     ("gps/fix", "ardusimple/fix"),
                     ("gps/filtered", "gps/filtered"),
                     ("odometry/gps", "odometry/gps"),
                     ("odometry/filtered", "odometry/global"),
                 ],
+                arguments=['--ros-args', '--log-level', 'INFO'],
             ),
     ])
